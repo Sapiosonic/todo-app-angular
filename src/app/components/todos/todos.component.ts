f136421d81dd9f7e42ad05e450546d73674302e1
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-09-11 17:21:05 -0300

    Create addTodo method and add it to the form

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -11,7 +11,7 @@ export class TodosComponent implements OnInit {
   todos: Todo[] = [];
 
   inputTodo:string = "";
-  
+
   constructor() { }
 
   ngOnInit(): void {
@@ -32,11 +32,18 @@ export class TodosComponent implements OnInit {
       if (i == id) v.completed = !v.completed;
 
       return v;
-    })
+    });
   }
 
   deleteTodo (id:number){
-    this.todos = this.todos.filter((v, i) => i !== id)
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo () {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
   }
 
 }
